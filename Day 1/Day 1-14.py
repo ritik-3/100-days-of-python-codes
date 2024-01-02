9411709f2d57add674aaf9f23fce8eb5644ec65b
Ritik <113279473+ritik-3@users.noreply.github.com>
2024-01-02 13:04:22 +0530

    Add files via upload

---
+++ Day 1/Day 1-14.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator")
+city = input("What's the Name of city you grew up in?" " \n ")
+pet = input("what's the Name of your first Pet?" " \n ")
+print("Your Band name could be " + city + " " + pet)
