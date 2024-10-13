cc821e3d0a1a0bcb53af553545af4dd8d6a7900f
Ritik <rebalerok1290@gmail.com>
2024-10-13 20:04:26 +0530

    Day 30 1

---
+++ Codes/Day 30/main.py
@@ -0,0 +1,17 @@
+#learning try except else finally
+
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key":"value"}
+    print(a_dictionary["sda"])
+except FileNotFoundError:
+    file = open("a_file.txt","w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("File was closed.")
