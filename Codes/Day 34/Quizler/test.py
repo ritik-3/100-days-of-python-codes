5168a43748f105415cabd761585e2f8b6b090a5b
Ritik <rebalerok1290@gmail.com>
2024-10-15 14:57:52 +0530

    Quizeler

---
+++ Codes/Day 34/Quizler/test.py
@@ -0,0 +1,5 @@
+import os
+
+# Check if images exist
+print(os.path.exists("Codes/Day 34/Quizler/images/true.png"))  # Should print True if file exists
+print(os.path.exists("Codes/Day 34/Quizler/images/false.png"))  # Should print True if file exists
