4a65de441d9a46dba456f92fe0a3ec1569de2774
Ritik <rebalerok1290@gmail.com>
2024-10-15 20:29:29 +0530

    weather

--- Codes/Day 35/main.py
+++ Codes/Day 35/main.py
@@ -1,29 +1,34 @@
 import requests
+from twilio.rest import Client
 
-OWM_Endpint = "https://api.openweathermap.org/data/2.5/forecast"
-api_key = "6a3f482683050c264a665c8a22ab3859"
-MY_LAT = 23.259933
-MY_LNG = 77.412613
-parameters = {
-    "lat" : MY_LAT,
-    "lon" : MY_LNG,
-    "appid" : api_key,
-    "cnt" : 4,
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = "__YOUR_OWM_API_KEY__"
+account_sid = "__YOUR_TWILIO_ACCOUNT_ID__"
+auth_token = "__YOUR_TWILIO_AUTH_TOKEN__"
+
+weather_params = {
+    "lat": 46.947975,
+    "lon": 7.447447,
+    "appid": api_key,
+    "cnt": 4,
 }
 
-
-responce = requests.get(OWM_Endpint, params=parameters)
-responce.raise_for_status()
-weather_data = responce.json()
-#print(weather_data["list"][0]["weather"][0])
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+# print(weather_data["list"][0]["weather"][0]["id"])
 
 will_rain = False
-
 for hour_data in weather_data["list"]:
     condition_code = hour_data["weather"][0]["id"]
-    
-    if int(condition_code)<700:
+    if int(condition_code) < 700:
         will_rain = True
-        
 if will_rain:
-    print("its gonna rain... :)")
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an ☔️",
+        from_="YOUR TWILIO VIRTUAL NUMBER",
+        to="YOUR TWILIO VERIFIED REAL NUMBER"
+    )
+    print(message.status)
