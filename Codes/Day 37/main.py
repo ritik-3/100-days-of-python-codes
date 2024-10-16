feb08d2ae8ff7327ae2a2b47c264e29311b9d12a
Ritik <rebalerok1290@gmail.com>
2024-10-16 22:29:42 +0530

    Pixela2.0

--- Codes/Day 37/main.py
+++ Codes/Day 37/main.py
@@ -1,7 +1,8 @@
 import requests
+import datetime
 
-USERNAME = "lululululu"
-TOKEN = "lalalalala"
+USERNAME = "lalalala"
+TOKEN = "lululululu"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
@@ -29,6 +30,30 @@ graph_config = {
 header = {
     "X-USER-TOKEN" : TOKEN
 }
+g_id = "graph1"
 
-responce_graph = requests.post(url=graph_endpoint, json=graph_config, headers=header)
-print(responce_graph.text)
+# responce_graph = requests.post(url=graph_endpoint, json=graph_config, headers=header)
+# print(responce_graph.text)
+
+today = datetime.datetime.now()
+
+
+pixel_config = {
+    "date" : today.strftime("%y%m%d"),
+    "quantity" : "5"
+}
+
+graph_pixel = f"{pixela_endpoint}/{USERNAME}/graphs/{g_id}"
+
+# responce_pixel = requests.post(url= graph_pixel,json=pixel_config,headers=header )
+# print(responce_pixel.text)
+
+graph_config_update = {
+    "unit" : "depression",
+    "color" : "ajisai"
+}
+
+graph_pixel_update = f"{pixela_endpoint}/{USERNAME}/graphs/{g_id}"
+
+update_pixel = requests.put(url=graph_pixel_update,json=graph_config_update,headers=header)
+print(update_pixel.text)
